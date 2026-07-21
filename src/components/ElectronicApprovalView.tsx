378b8501e878f675eb39c1d5ed0afdeff4f7d6c6
parkhy5454 <parkhy5454@gmail.com>
2026-07-21 20:53:33 +0900

    Add files via upload

--- src/components/ElectronicApprovalView.tsx
+++ src/components/ElectronicApprovalView.tsx
@@ -993,11 +993,13 @@ export const ElectronicApprovalView: React.FC<Props> = ({ currentUser }) => {
       <table style="border-collapse: collapse; width:100%; height:100%; border: 2px solid #000000;"><tr>
       <td style="box-sizing: border-box; padding: 10mm; mso-padding-alt: 28pt 28pt 28pt 28pt; vertical-align: top;">
       <div style="text-align:center; margin-bottom:16px;">
-        <span style="font-size:18pt; font-weight:bold;">휴가 신청서</span>
-        <div style="width:160px; margin:6px auto 0 auto;">
-          <div style="border-bottom:2px solid #000000; height:2px;"></div>
-          <div style="border-bottom:2px solid #000000; height:2px; margin-top:2px;"></div>
-        </div>
+        <span style="font-size:18pt; font-weight:bold;">휴가 신청서</span><br/>
+        <table style="border-collapse: collapse; margin: 6px auto 0 auto;" cellpadding="0" cellspacing="0"><tr>
+          <td style="width:160px; border-bottom:2px solid #000000; font-size:1px; line-height:2px;">&nbsp;</td>
+        </tr></table>
+        <table style="border-collapse: collapse; margin: 2px auto 0 auto;" cellpadding="0" cellspacing="0"><tr>
+          <td style="width:160px; border-bottom:2px solid #000000; font-size:1px; line-height:2px;">&nbsp;</td>
+        </tr></table>
       </div>
       ${bodyHtml}
       </td>
