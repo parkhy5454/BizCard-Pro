a5c69177dfbd5c8eff38fbc764707ed9d1775fcf
parkhy5454 <parkhy5454@gmail.com>
2026-08-05 15:52:31 +0900

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -369,6 +369,7 @@ export default function App() {
                 }}
                 onDeleteContact={handleDeleteCard}
                 onNavigateToProjects={() => setActiveTab('projects')}
+                onAddCallHistory={handleAddCallHistory}
               />
             )}
 
